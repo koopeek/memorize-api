189abe2bb76a60cb7d2fab4f12ef093e86b07c73
koopeek <slawekkopaczewski@gmail.com>
2020-07-13 16:30:17 +0200

    Add bscrypt for hash passwords purpose

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,18 +1,22 @@
 import { Request, Response } from "express";
-import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 import { createUser, saveUser, getUserByEmail } from "../services/userService";
+import { IUser } from "../interfaces/IUser";
 
 const register = async (req: Request, res: Response) => {
   const { email, password } = req.body;
   try {
-    const user = getUserByEmail(email);
+    const user: IUser = await getUserByEmail(email);
 
     if (user) {
-      res.status(400).send("User already exists.");
+      res.status(400).send("User with given e-mail address already exists.");
     }
 
-    const newUser = await createUser(email, password);
-    const newUserSaved = await saveUser(newUser);
+    const salt: string = await bcrypt.genSalt(10);
+    const hashedPassword: string = await bcrypt.hash(password, salt);
+
+    const newUser: IUser = await createUser(email, hashedPassword);
+    const newUserSaved: IUser = await saveUser(newUser);
 
     res.status(200).send(newUserSaved);
   } catch (e) {
